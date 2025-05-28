bff48e976685ecdda814cfb3ce2f683abf963690
faustopavan12 <faustopavan2003@gmail.com>
2025-05-28 11:30:17 -0300

    hash

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
 class Venta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
